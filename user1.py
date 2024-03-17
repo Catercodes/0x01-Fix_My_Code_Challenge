d2ac4688bac545dc9f36e59e791ac41b208459a1
Catercodes <chrisadamsadewuni@gmail.com>
2024-03-17 17:18:59 +0100

    Based on the concept of OOP i maunipulated the setter and getter

--- user1.py
+++ user1.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""
+""" 
 User class
 """
-
 
 class User():
     """ Documentation """
@@ -22,10 +21,11 @@ class User():
         if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
-
-
+   
+    
 if __name__ == "__main__":
 
     u = User()
     u.email = "john@snow.com"
     print(u.email)
+
